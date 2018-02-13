2e78ac1d137edc2350a9bfeeaa21b55cbad4f5d6
sasquad92 <krzysztof.sommerrey@gmail.com>
2018-02-13 19:36:17 +0100

    using boltdb to store blocks from blockchain

--- main.go
+++ main.go
@@ -1,23 +1,18 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
-
 func main() {
 	bc := NewBlockchain()
 
 	bc.AddBlock("Send 1 BC to Kevin")
 	bc.AddBlock("Send 1 BC to Eva")
 
-	for _, block := range bc.blocks {
-		pow := NewProofOfWork(block)
+	// for _, block := range bc.blocks {
+	// 	pow := NewProofOfWork(block)
 
-		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %x\n", block.Hash)
-		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
-		fmt.Println()
-	}
+	// 	fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
+	// 	fmt.Printf("Data: %s\n", block.Data)
+	// 	fmt.Printf("Hash: %x\n", block.Hash)
+	// 	fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+	// 	fmt.Println()
+	// }
 }
