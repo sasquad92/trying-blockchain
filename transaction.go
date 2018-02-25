4a4e8e97db58c591e170c9b728a30285076b86f4
sasquad92 <krzysztof.sommerrey@gmail.com>
2018-02-25 19:04:06 +0100

    adding transactions - structs

---
+++ transaction.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+const subsidy = 10
+
+type Transaction struct {
+	ID   []byte
+	Vin  []TXInput
+	Vout []TXOutput
+}
+
+type TXOutput struct {
+	Value        int
+	ScriptPubKey string
+}
+
+type TXInput struct {
+	Txid      []byte
+	Vout      int
+	ScriptSig string
+}
+
+func NewCoinbaseTX(to, data string) *Transaction {
+	if data == "" {
+		data = fmt.Sprintf("Reward to %s", to)
+	}
+
+	txin := TXInput{[]byte{}, -1, data}
+	txout := TXOutput{subsidy, to}
+	tx := Transaction{nil, []TXInput{txin}, []TXOutput{txout}}
+
+	return &tx
+}
